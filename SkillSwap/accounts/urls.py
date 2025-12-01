afd7ec4681876c6f3dd4cf946bc7b420cda42171
Norah <norahwasr@gmail.com>
2025-12-01 18:54:24 +0300

    register feature

---
+++ SkillSwap/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("signup/", views.signup_view, name="signup_view"),
+    path("signin/", views.signin_view, name="signin_view"),
+    path("logout/", views.log_out_view, name="log_out_view"),
+]
--- SkillSwap/accounts/views.py
+++ SkillSwap/accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def signup_view(request : HttpRequest):
+    
+    if request.method == "POST":
+        try:
+            new_user = User.objects.create_user(
+                username = request.POST["username"],
+                password = request.POST["password"],
+                email = request.POST["email"],
+                first_name = request.POST["first_name"],
+                last_name = request.POST["last_name"],
+                )
+            new_user.save()
+            messages.success(request, "Registered User Successfuly", "alert-success")
+            return redirect("accounts:signin_view")
+        except Exception as e: 
+            print(e)
+    
+    return render(request, "accounts/signup.html", {})
+
+
+def signin_view(request : HttpRequest):
+    
+    if request.method == "POST":
+        user = authenticate(request, username = request.POST["username"],
+                password = request.POST["password"])
+        
+        if user:
+            login(request, user)
+            messages.success(request, "logged in successfuly", "alert-success")
+            return redirect(request.GET.get("next", "/"))
+        else:
+            messages.error(request, "Please try again, your info is wrong", "alert-danger")
+     
+    return render(request, "accounts/signin.html", {})
+
+
+def log_out_view(request : HttpRequest):
+    
+    logout(request)
+    messages.success(request, "logged out successfuly", "alert-success")
+    return redirect("main:home_view")
